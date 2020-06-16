208b6c33e1bafd55b362cc7f2434c2c2d3536e29
joshua <joshuasylvester289@gmail.com>
2020-06-16 11:01:41 -0700

    Added the exit condition

--- AssaultCubeUnlimitedAmmo/AssaultCubeUnlimitedAmmo.cpp
+++ AssaultCubeUnlimitedAmmo/AssaultCubeUnlimitedAmmo.cpp
@@ -2,47 +2,41 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include <Windows.h>
 
 
 int main()
 {
     int PID;
-    std::cout << "What is the PID: ";
-    std::cin >> PID;
+    int setAmmo = 20;
+    int ammo;
 
     uintptr_t playerBaseAddress = 0x50F4F4;
-
     uintptr_t ammoOffset = 0x150;
+    uintptr_t pointerToAmmo;
 
-    int setAmmo = 20;
+    std::cout << "What is the PID: ";
+    std::cin >> PID;
 
     HANDLE assaultCube = OpenProcess(PROCESS_ALL_ACCESS,FALSE,PID);
 
 
-    uintptr_t pointerToAmmo;
-
-    int ammo;
-
     ReadProcessMemory(assaultCube, (LPCVOID)playerBaseAddress, &pointerToAmmo, sizeof(uintptr_t), NULL);
-    ReadProcessMemory(assaultCube, (LPCVOID)(pointerToAmmo + ammoOffset), &ammo, sizeof(int), NULL);
-    WriteProcessMemory(assaultCube, (LPVOID)(pointerToAmmo + ammoOffset), &setAmmo, sizeof(int), NULL);
-    std::cout << ammo;
+    while (GetAsyncKeyState(0x41) == 0)
+    {
+        
+        if (WriteProcessMemory(assaultCube, (LPVOID)(pointerToAmmo + ammoOffset), &setAmmo, sizeof(int), NULL) == FALSE) {
+            std::cout << "Error Code: " << GetLastError() << std::endl;
+            EXIT_FAILURE;
 
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-
-    //WriteProcessMemory(assaultCube,ammoAddress, &setAmmo, sizeof(int), NULL);
-
+    }
+    
     CloseHandle(assaultCube);
+    EXIT_SUCCESS;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
